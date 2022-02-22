eca9460a8a9a143518a910cc4cf64828f5b34def
kamal-kishor-rajput <90085564+kamal-kishor-rajput@users.noreply.github.com>
2022-02-23 02:37:05 +0530

    footer complete done , almost header done

---
+++ components/footer.js
@@ -0,0 +1,81 @@
+
+
+let footer = () => {
+    return `
+     <div id="footer-section">
+        <div class="column">
+          <ul>
+            <h3>CUSTOMER SERVICE</h3>
+            <li>Contact Us</li>
+            <li>Email Preferences</li>
+            <li>Track your order</li>
+            <li>Return & Exchanges</li>
+            <li>Help Topics</li>
+            <li>Shopping Information</li>
+            <li>Give Us Feedback</li>
+            <li>International Orders</li>
+          </ul>
+        </div>
+        <div class="column">
+          <ul>
+            <h3>ABOUT Us</h3>
+            <li>Our Story</li>
+            <li>Careers</li>
+            <li>Press</li>
+            <li>Influencers</li>
+            <li>Find a Store</li>
+          </ul>
+          <ul>
+            <h3>B2B PROGRAMS</h3>
+            <li>Overview</li>
+            <li>Contract</li>
+            <li>Trade</li>
+            <li>Corporate</li>
+          </ul>
+        </div>
+        <div class="column">
+          <ul>
+            <h3>DSIGN SERVICES</h3>
+            <li>Free Interior Design</li>
+            <li>Installation Services</li>
+            <li>Start a Design Chat</li>
+          </ul>
+          <ul>
+            <h3>RESOURCES</h3>
+            <li>Pottery Barn Credit Card</li>
+            <li>Request a Catalog</li>
+            <li>Pay Bill Online</li>
+            <li>Address Change</li>
+            <li>Gift Cards</li>
+            <li>View Online Catalog</li>
+          </ul>
+        </div>
+        <div class="column">
+          <ul>
+            <p>Join our VIP list for inspiration, new arrivals & more.</p>
+            <div id="footer-email-sign-up">
+              <input type="email" />
+              <button>SIGNUP</button>
+            </div>
+            <p>FOLLOW US</p>
+            <ul>
+              <li>insta</li>
+              <li>facebook</li>
+              <li>pinterest</li>
+              <li>youtube</li>
+              <li>twitter</li>
+            </ul>
+          </ul>
+        </div>
+      </div>
+      <p>
+        © 2022 Williams-Sonoma, Inc. All Rights Reserved | Terms & Conditions
+        (Updated April 2020) | Accessibility | Privacy Policy (Updated January
+        2022) | Legal Statement
+      </p>
+    
+    
+    `;
+}
+
+export default footer;
---
+++ scripts/index.js
@@ -0,0 +1,7 @@
+//code for importing footer
+
+import footer from "/components/footer.js";
+let ft_container = document.getElementById("ft-container");
+ft_container.innerHTML = footer();
+
+//code for importing footer
