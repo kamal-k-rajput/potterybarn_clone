c048b0817839dac372316ce7af9fcda2fbe7fa1d
Sumit Ranjan <sumitranjan.ind@gmail.com>
2022-02-25 15:04:45 +0530

    car page css update

--- scripts/sofa.js
+++ scripts/sofa.js
@@ -1,3 +1,19 @@
+//code for importing footer
+
+import footer from "/components/footer.js";
+let ft_container = document.getElementById("ft-container");
+ft_container.innerHTML = footer();
+
+//code for importing footer
+
+
+//code for importing header
+import header from "/components/header.js";
+let header_container = document.getElementById("header-main-container");
+header_container.innerHTML = header();
+
+
+//code for importing header
 
 
 
