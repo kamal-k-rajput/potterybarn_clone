3b06c7050d25d748bf96dc5cb9a81d2c34af30e6
kamal-kishor-rajput <90085564+kamal-kishor-rajput@users.noreply.github.com>
2022-04-02 14:31:05 +0530

    some files deleted

--- src/configs/db.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const mongoose = require("mongoose");
-
-module.exports = () => {
-  return mongoose.connect(
-    "mongodb+srv://kamalkishor:Kamak-1234@cluster0.pg1tz.mongodb.net/potterybarn-clone?retryWrites=true&w=majority"
-  );
-};
